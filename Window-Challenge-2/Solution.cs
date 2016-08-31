b24dfeb6f2afd5304074e5288bb366374b6b83f9
jmprsp <root@jmprsp.com>
2016-08-31 16:13:11 +0800

    Windows Challenge 2

---
+++ Window-Challenge-2/Solution.cs
@@ -0,0 +1,122 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+namespace palo_2
+{
+    class Program
+    {
+        private string[] thangs = new string[] { "", "", "", "", "", "", "", "", "", "", "" };
+        static void Main(string[] args)
+        {
+            new Program();
+        }
+
+        Program()
+        {
+             this.thangs[3] = @" _|| || | |_   ___ `.  | || |      _       | || |    \_ `.    " + this.thangs[3];
+                this.thangs[10] = this.thangs[10] + " '----------------'  '----------------'  '----------------'  '";
+                this.thangs[5] = "|  | || |   | |    | | | || |     | |      | || |       > >   " + this.thangs[5];
+                this.thangs[7] = "   | || | |________.'  | || |     |_|      | || |    /__.'    " + this.thangs[7];
+                this.thangs[9] = "---' || '--------------' || '--------------' || '-------------" + this.thangs[9];
+                this.thangs[0] = this.thangs[0] + " .----------------.  .----------------.  .-----------------. .";
+                this.thangs[2] = this.thangs[2] + "| |   ______     | || |      __      | || | ____  _____  | || ";
+                this.thangs[1] = "---. || .--------------. || .--------------. || .-------------" + this.thangs[1];
+                this.thangs[8] = this.thangs[8] + "| |              | || |              | || |              | || ";
+                this.thangs[4] = this.thangs[4] + @"| |    | |__) |  | || |    / /\ \    | || |  |   \ | |   | || ";
+                this.thangs[6] = this.thangs[6] + @"| |   _| |_      | || | _/ /    \ \_ | || | _| |_\   |_  | || ";
+           
+            this.thangs[6] = this.thangs[6] + "|     | |_     | || |   _| |__) |  | || |      _| |_   | || | ";
+                this.thangs[10] = this.thangs[10] + "----------------'  '----------------'  '----------------'  '--";
+                this.thangs[1] = "-----. || .--------------. || .--------------. || .-----------" + this.thangs[1];
+                this.thangs[4] = this.thangs[4] + "|     | |      | || |    | |_) |   | || |  | |__| |_   | || | ";
+                this.thangs[9] = "-----' || '--------------' || '--------------' || '-----------" + this.thangs[9];
+                this.thangs[7] = "_    | || |  |________|  | || |   `.____.'   | || |    `.__.' " + this.thangs[7];
+                this.thangs[3] = "     | || |  |_   _|     | || |   .'    `.   | || ||_   _||_  " + this.thangs[3];
+                this.thangs[0] = this.thangs[0] + "----------------.  .----------------.  .----------------.  .--";
+                this.thangs[5] = "     | || |    | |   _   | || |  | |    | |  | || |  | '    ' " + this.thangs[5];
+                this.thangs[2] = this.thangs[2] + "|       __     | || |   ______     | || |   _    _     | || | ";
+                this.thangs[8] = this.thangs[8] + "|              | || |              | || |              | || | ";
+               
+            this.thangs[4] = this.thangs[4] + @"   | |_) |   | || |    | |_) |   | || |   \ \  / /   | || |   ";
+                this.thangs[2] = this.thangs[2] + "  ______     | || |   ______     | || |  ____  ____  | || |   ";
+                this.thangs[3] = "       | || |  |  _____|   | || |   .'    '.   | || |         " + this.thangs[3];
+                this.thangs[9] = "-------' || '--------------' || '--------------' || '---------" + this.thangs[9];
+                this.thangs[8] = this.thangs[8] + "             | || |              | || |              | || |  |";
+                this.thangs[0] = this.thangs[0] + "--------------.  .----------------.  .----------------.  .----";
+                this.thangs[1] = "-------. || .--------------. || .--------------. || .---------" + this.thangs[1];
+                this.thangs[6] = this.thangs[6] + "  _| |__) |  | || |   _| |__) |  | || |    _|  |_    | || |   ";
+                this.thangs[7] = @"___    | || |   \______.'  | || |   '.____.'   | || |   ______" + this.thangs[7];
+                this.thangs[10] = this.thangs[10] + "--------------'  '----------------'  '----------------'  '----";
+                this.thangs[5] = "       | || |  '_.____''.  | || |  | |    | |  | || |         " + this.thangs[5];
+                
+
+                this.thangs[2] = this.thangs[2] + "           | || |  ____  ____  | || |    ______    | || |   __";
+                this.thangs[9] = "---------' || '--------------' || '--------------' || '-------" + this.thangs[9];
+                this.thangs[3] = "  _|     | || |  |_   _|     | || |  |  _____|   | || |       " + this.thangs[3];
+                this.thangs[8] = this.thangs[8] + "_______|   | || |              | || |              | || |     ";
+                this.thangs[10] = this.thangs[10] + "------------'  '----------------'  '----------------'  '------";
+                this.thangs[7] = @"______|  | || |  |________|  | || |   \______.'  | || |   ____" + this.thangs[7];
+                this.thangs[0] = this.thangs[0] + "------------.  .----------------.  .----------------.  .------";
+                this.thangs[5] = " |   _   | || |    | |   _   | || |  '_.____''.  | || |       " + this.thangs[5];
+                this.thangs[4] = this.thangs[4] + @"           | || |   \ \  / /   | || |   `'  __) |  | || |    |";
+                this.thangs[1] = "---------. || .--------------. || .--------------. || .-------" + this.thangs[1];
+                this.thangs[6] = this.thangs[6] + @"           | || |    _|  |_    | || |  | \____) |  | || |   _|";
+               
+            this.thangs[7] = @"_______    | || |   |______|   | || |   \______.'  | || |  |__" + this.thangs[7];
+                this.thangs[5] = @"           | || |    \ \/ /    | || |   _  |__ '.  | || |    |" + this.thangs[5];
+                this.thangs[6] = this.thangs[6] + @" |__/ |  | || |   _| |__/ |  | || |  | \____) |  | || |       ";
+                this.thangs[4] = this.thangs[4] + " |       | || |    | |       | || |  | |____     | || |       ";
+                this.thangs[2] = this.thangs[2] + "___      | || |   _____      | || |   _______    | || |       ";
+                this.thangs[10] = this.thangs[10] + "----------'  '----------------'  '----------------'  '--------";
+                this.thangs[8] = this.thangs[8] + "         | || |              | || |              | || |  |____";
+                this.thangs[3] = "           | || | |_  _||_  _| | || |   / ____ `.  | || |  |_ " + this.thangs[3];
+                this.thangs[1] = "-----------. || .--------------. || .--------------. || .-----" + this.thangs[1];
+                this.thangs[9] = "-----------' || '--------------' || '--------------' || '-----" + this.thangs[9];
+                this.thangs[0] = this.thangs[0] + "----------.  .----------------.  .----------------.  .--------";
+               
+
+                this.thangs[1] = "-------------. || .--------------. || .--------------. || .---" + this.thangs[1];
+                this.thangs[8] = this.thangs[8] + "___|   | || |              | || |              | || |  |______";
+                this.thangs[7] = " |_______/   | || |  |_______/   | || |   |______|   | || |   " + this.thangs[7];
+                this.thangs[9] = "-------------' || '--------------' || '--------------' || '---" + this.thangs[9];
+                this.thangs[3] = @" |_   _ \    | || |  |_   _ \    | || | |_  _||_  _| | || |   " + this.thangs[3];
+                this.thangs[10] = this.thangs[10] + "--------'  '----------------'  '----------------'  '----------";
+                this.thangs[2] = this.thangs[2] + "       | || |   _______    | || |     ____     | || |         ";
+                this.thangs[5] = @"   |  __'.   | || |    |  __'.   | || |    \ \/ /    | || |   " + this.thangs[5];
+                this.thangs[6] = this.thangs[6] + @"       | || |  | \____) |  | || |  |  `--'  |  | || |         ";
+                this.thangs[4] = this.thangs[4] + "       | || |  | |____     | || |  |  .--.  |  | || |         ";
+                this.thangs[0] = this.thangs[0] + "--------.  .----------------.  .----------------.  .----------";
+            
+            this.thangs[6] = this.thangs[6] + @"     | || |   _| |__/ |  | || |  \  `--'  /  | || |   \ `--' /";
+                this.thangs[3] = @"|     .' _/    | || |  |_   _ \    | || |  | |  | |    | || | " + this.thangs[3];
+                this.thangs[2] = this.thangs[2] + "     | || |   _____      | || |     ____     | || | _____  ___";
+                this.thangs[5] = "|    < <       | || |    |  __'.   | || |  |____   _|  | || | " + this.thangs[5];
+                this.thangs[10] = this.thangs[10] + "------'  '----------------'  '----------------'  '------------";
+                this.thangs[8] = this.thangs[8] + "_|   | || |              | || |              | || |           ";
+                this.thangs[1] = ".--------------. || .--------------. || .--------------. || .-" + this.thangs[1];
+                this.thangs[9] = "'--------------' || '--------------' || '--------------' || '-" + this.thangs[9];
+                this.thangs[4] = this.thangs[4] + @"     | || |    | |       | || |  /  .--.  \  | || |  | |    | ";
+                this.thangs[7] = @"|     `.__\    | || |  |_______/   | || |     |_____|  | || | " + this.thangs[7];
+                this.thangs[0] = this.thangs[0] + "------.  .----------------.  .----------------.  .------------";
+               
+            this.thangs[10] = this.thangs[10] + "----'  '----------------'  '----------------'  '--------------";
+                this.thangs[4] = this.thangs[4] + @"|  | || |   | |   `. \ | || |     | |      | || |      | |    ";
+                this.thangs[3] = @"| |  |_   __ \   | || |     /  \     | || ||_   \|_   _| | || " + this.thangs[3];
+                this.thangs[2] = this.thangs[2] + "__ | || |  ________    | || |              | || |     __      ";
+                this.thangs[0] = this.thangs[0] + "----.  .----------------.  .----------------.  .--------------";
+                this.thangs[6] = this.thangs[6] + "   | || |  _| |___.' / | || |     | |      | || |     _| |    ";
+                this.thangs[9] = "| '--------------' || '--------------' || '--------------' || " + this.thangs[9];
+                this.thangs[5] = @"| |    |  ___/   | || |   / ____ \   | || |  | |\ \| |   | || " + this.thangs[5];
+                this.thangs[8] = this.thangs[8] + "   | || |              | || |     (_)      | || |             ";
+                this.thangs[7] = @"| |  |_____|     | || ||____|  |____|| || ||_____|\____| | || " + this.thangs[7];
+                this.thangs[1] = "| .--------------. || .--------------. || .--------------. || " + this.thangs[1];
+
+                System.IO.StreamWriter file = new System.IO.StreamWriter(@"WriteLines2.txt");
+                for (int i = 0; i < 10; i++)
+                    file.WriteLine(thangs[i]);
+        }
+    }
+}
