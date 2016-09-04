fc732c153720029afa24a11ca6d40190b56b6809
jmprsp <root@jmprsp.com>
2016-09-04 23:22:01 +0800

    challenge 5

---
+++ Window-Challenge-5/RGB Source/RGB/Properties/Resources.cs
@@ -0,0 +1,48 @@
+﻿namespace RGB.Properties
+{
+    using System;
+    using System.CodeDom.Compiler;
+    using System.ComponentModel;
+    using System.Diagnostics;
+    using System.Globalization;
+    using System.Resources;
+    using System.Runtime.CompilerServices;
+
+    [GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0"), DebuggerNonUserCode, CompilerGenerated]
+    internal class Resources
+    {
+        private static CultureInfo resourceCulture;
+        private static System.Resources.ResourceManager resourceMan;
+
+        internal Resources()
+        {
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        internal static CultureInfo Culture
+        {
+            get
+            {
+                return resourceCulture;
+            }
+            set
+            {
+                resourceCulture = value;
+            }
+        }
+
+        [EditorBrowsable(EditorBrowsableState.Advanced)]
+        internal static System.Resources.ResourceManager ResourceManager
+        {
+            get
+            {
+                if (resourceMan == null)
+                {
+                    resourceMan = new System.Resources.ResourceManager("RGB.Properties.Resources", typeof(Resources).Assembly);
+                }
+                return resourceMan;
+            }
+        }
+    }
+}
+
